8d38b35aaab7fe8ec8c8f46eb005d1d3cedf2503
mr-new-0509 <cliffahleongtech@gmail.com>
2023-07-13 11:49:14 +0900

    AAVE

---
+++ scripts/verifyMockAggregator.js
@@ -0,0 +1,21 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// will compile your contracts, add the Hardhat Runtime Environment's members to the
+// global scope, and execute the script.
+const hre = require("hardhat");
+
+async function main() {
+  await hre.run("verify:verify", {
+    address: "0xc9c65e8D77ef6a27801870C80010A1aa79EaD079",
+    constructorArguments: [1861],
+  });
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
